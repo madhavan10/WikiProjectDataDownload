ad52d6a54a88cad9f34fa21c719a1c67d4250640
Madhavan Somanathan <madhavansomanathan@gmail.com>
2021-12-17 12:35:51 +0530

    Loop thru all top editors

--- get_join_dates_top_10000.py
+++ get_join_dates_top_10000.py
@@ -29,48 +29,55 @@ for i in dfProjectMembership.index:
         usernameToProjectMembership[user] = []
     usernameToProjectMembership[user].append(project)
 
-userLookupId = 12108
-contribsCsvPath = os.path.join(topEditorsRootDir, "contributions", str(userLookupId) + ".csv")
-user = userLookup[userLookupId]
-if user in usernameToProjectMembership.keys():
-    projectList = usernameToProjectMembership[user]
-    regexList = []
-    ifMatched = []
-    joinDates = []
-    titles = []
-    matchedProjects = []
-    for project in projectList:
-        regex = "Wikipedia:WikiProject (" + project + "|" + project + ".*(members?|participants?).*)"
-        regexList.append(regex)
-        ifMatched.append(False)
-    with open(contribsCsvPath, "r", newline = "", encoding = "utf-8") as contribsCsv:
-        reader = csv.reader(contribsCsv)
-        for row in reader:
-            TIMESTAMP_INDEX = 2
-            TITLE_INDEX = 5
-            NS_INDEX = 3
-            if row[NS_INDEX] != "4":
-                continue
-            title = row[TITLE_INDEX]
-            for i in range(len(regexList)):
-                if ifMatched[i]:
+for file in os.listdir(os.path.join(topEditorsRootDir, "contributions")):
+    splitFilename = file.rsplit(".", maxsplit = 1)
+    if splitFilename[-1] != "csv":
+        continue
+    joinDatesDfPath = os.path.join(topEditorsRootDir, "projectJoinDates", file)
+    unmatchedProjectsDfPath = os.path.join(topEditorsRootDir, "unmatchedProjects", file)
+    if os.path.exists(joinDatesDfPath) and os.path.exists(unmatchedProjectsDfPath):
+        continue
+    userLookupId = int(splitFilename[0])
+    contribsCsvPath = os.path.join(topEditorsRootDir, "contributions", file)
+    user = userLookup[userLookupId]
+    if user in usernameToProjectMembership.keys():
+        print(user, file)
+        projectList = usernameToProjectMembership[user]
+        regexList = []
+        ifMatched = []
+        joinDates = []
+        titles = []
+        matchedProjects = []
+        for project in projectList:
+            regex = "Wikipedia:WikiProject (" + project + "|" + project + ".*(members?|participants?).*)"
+            regexList.append(regex)
+            ifMatched.append(False)
+        with open(contribsCsvPath, "r", newline = "", encoding = "utf-8") as contribsCsv:
+            reader = csv.reader(contribsCsv)
+            for row in reader:
+                TIMESTAMP_INDEX = 2
+                TITLE_INDEX = 5
+                NS_INDEX = 3
+                if row[NS_INDEX] != "4":
                     continue
-                match = re.fullmatch(regexList[i], title, flags = re.IGNORECASE)
-                if match:
-                    joinDates.append(row[TIMESTAMP_INDEX])
-                    titles.append(title)
-                    matchedProjects.append(projectList[i])
-                    ifMatched[i] = True
-    joinDatesDf = pd.DataFrame({"joinDate": joinDates, "WikiProject": matchedProjects, "pageTitle": titles})
-    joinDatesDf.to_csv(os.path.join(topEditorsRootDir, "projectJoinDates", str(userLookupId) + ".csv")\
-                       , index = False, encoding = "utf-8")
-    unmatchedProjects = []
-    for i in range(len(projectList)):
-        if ifMatched[i] == False:
-            unmatchedProjects.append(projectList[i])
-    unmatchedProjectsDf = pd.DataFrame({"user": user, "unmatchedWikiProject": unmatchedProjects})
-    unmatchedProjectsDf.to_csv(os.path.join(topEditorsRootDir, "unmatchedProjects", str(userLookupId) + ".csv")\
-                               , index = False, encoding = "utf-8")
+                title = row[TITLE_INDEX]
+                for i in range(len(regexList)):
+                    if ifMatched[i]:
+                        continue
+                    match = re.fullmatch(regexList[i], title, flags = re.IGNORECASE)
+                    if match:
+                        joinDates.append(row[TIMESTAMP_INDEX])
+                        titles.append(title)
+                        matchedProjects.append(projectList[i])
+                        ifMatched[i] = True
+        joinDatesDf = pd.DataFrame({"joinDate": joinDates, "WikiProject": matchedProjects, "pageTitle": titles})
+        joinDatesDf.to_csv(joinDatesDfPath, index = False, encoding = "utf-8")
+        unmatchedProjects = []
+        for i in range(len(projectList)):
+            if ifMatched[i] == False:
+                unmatchedProjects.append(projectList[i])
+        unmatchedProjectsDf = pd.DataFrame({"user": user, "unmatchedWikiProject": unmatchedProjects})
+        unmatchedProjectsDf.to_csv(unmatchedProjectsDfPath, index = False, encoding = "utf-8")
 
             
             
