bc852e524b582d1e82ed1928a3b7709f6de0d29d
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-02-01 13:05:56 +0530

    added a usernames invalid csv writer

--- get_up_to_date_usernames_top_10000_union_selenium.py
+++ get_up_to_date_usernames_top_10000_union_selenium.py
@@ -44,6 +44,9 @@ usernamesMapPath = "/home/madhavso/wikipedia_data/user_lists/usernamesMapTopEdit
 usernamesMapPathExists = os.path.exists(usernamesMapPath)
 failedQueriesPath = "/home/madhavso/wikipedia_data/user_lists/failedQueriesTopEditors.txt"
 
+usernamesInvalidPath = "/home/madhavso/wikipedia_data/user_lists/usernamesInvalidTopEditors.csv"
+
+
 with open(usernamesMapPath, "a", newline = "", encoding = "utf-8") as usernamesMapCsv:
     writer = csv.writer(usernamesMapCsv, quoting = csv.QUOTE_MINIMAL)
     if not usernamesMapPathExists:
@@ -53,28 +56,34 @@ with open(usernamesMapPath, "a", newline = "", encoding = "utf-8") as usernamesM
         for i in usernamesMapDf.index:
             usernamesMap[usernamesMapDf["username"][i]] = usernamesMapDf["mostRecentUsername"][i]
     
-    with open(failedQueriesPath, "w", newline="", encoding = "utf-8") as failedQueriesLog:        
-        i = 0
-        for file in os.listdir():
-            split = file.rsplit(".", maxsplit = 1)
-            if split[-1] != "csv":
-                continue
-            i += 1
-            username = lookupIdToName[int(split[0])]
-            if username in usernamesMap.keys():
-                continue
-            try:
-                mostRecentUsername = getRecentUsername(username, driver)
-            except KeyboardInterrupt:
-                print("Stopped execution on ", file, username)
-                raise
-            except:
-                failedQueriesLog.write(split[0] + "\n")
-                failedQueriesLog.write(username + "\n")
-                traceback.print_tb(sys.exc_info()[2], file = failedQueriesLog)
-                continue
-            usernamesMap[username] = mostRecentUsername
-            writer.writerow([username, mostRecentUsername])
-            print(i, split[0], username, mostRecentUsername, sep = " : ")
-
-
+    with open(usernamesInvalidPath, "w", newline = "", encoding = "utf-8") as usernamesInvalidCsv:
+        writerInvalidUsernames = csv.writer(usernamesInvalidCsv, quoting = csv.QUOTE_MINIMAL)
+        writerInvalidUsernames.writerow(["username"])
+    
+        with open(failedQueriesPath, "w", newline="", encoding = "utf-8") as failedQueriesLog:        
+            i = 0
+            for file in os.listdir():
+                split = file.rsplit(".", maxsplit = 1)
+                if split[-1] != "csv":
+                    continue
+                i += 1
+                username = lookupIdToName[int(split[0])]
+                if username in usernamesMap.keys():
+                    continue
+                try:
+                    mostRecentUsername = getRecentUsername(username, driver)
+                except KeyboardInterrupt:
+                    print("Stopped execution on ", file, username)
+                    raise
+                except:
+                    failedQueriesLog.write(split[0] + "\n")
+                    failedQueriesLog.write(username + "\n")
+                    traceback.print_tb(sys.exc_info()[2], file = failedQueriesLog)
+                    continue
+                if mostRecentUsername == None:
+                    writerInvalidUsernames.writerow([username])
+                else:
+                    writer.writerow([username, mostRecentUsername])
+                print(i, split[0], username, mostRecentUsername, sep = " : ")
+    
+    
