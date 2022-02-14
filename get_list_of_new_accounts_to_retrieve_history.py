9b1fa6eccd5b9394a08e59b2445286909faf2f74
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-02-14 12:28:07 +0530

    get list of new accounts to retrieve contribs for

---
+++ get_list_of_new_accounts_to_retrieve_history.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 14 11:41:43 2022
+
+@author: madha
+"""
+import pandas as pd
+import requests
+import csv
+
+lookupPath = "/home/madhavso/wikipedia_data/user_lists/1-10000_union_lookup_updated.csv"
+lookupDf = pd.read_csv(lookupPath, encoding = "utf-8")
+lookup = {}
+for i in lookupDf.index:
+    lookup[lookupDf["user"][i]] = lookupDf["userNumber"][i]
+
+usersPath = "/home/madhavso/wikipedia_data/user_lists/users_to_retrieve.csv"
+usersDf = pd.read_csv(usersPath, encoding = "utf-8")
+
+usersToRetrieve = []
+usersNotToRetrieve = []
+
+S = requests.Session()
+URL = "https://en.wikipedia.org/w/api.php"
+
+for i in usersDf.index:
+    oldUsername = usersDf["oldUsername"][i]
+    newUsername = usersDf["mostRecentUsername"][i]
+    filename = str(lookup[oldUsername]) + ".csv"
+    with open(filename, newline = "", encoding = "utf-8") as f:
+        reader = csv.reader(f)
+        #skip header
+        next(reader)
+        oldUserId = next(reader)[0]
+        
+    PARAMS = {
+           "action": "query",
+           "format": "json",
+           "list": "users",
+           "usprop": "",
+           "ususers": newUsername
+        }
+    
+    R = S.get(url = URL, params = PARAMS)
+    json = R.json()
+    user = json["query"]["users"][0]
+    newUserId = str(user["userid"])
+    
+    if oldUserId == newUserId:
+        usersNotToRetrieve.append(newUsername)
+    else:
+        usersToRetrieve.append(newUsername)
+
+outDf = pd.DataFrame({"usersToRetrieve":usersToRetrieve})
+outDf.to_csv("/home/madhavso/wikipedia_data/user_lists/usersToRetrieve.csv", index = False, encoding = "utf-8")
+
+with open("/home/madhavso/wikipedia_data/user_lists/usersNotToRetrieve.txt", "w", encoding = "utf-8") as oF:
+    for u in usersNotToRetrieve:
+        f.write(u + "\n")
+
+        
