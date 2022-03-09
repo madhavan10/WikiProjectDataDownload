1d3318f6c24f9c1d6272cc4c0e74f6d998e557a5
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-03-09 14:48:48 +0530

    made a URL-safe version of username to be used if username has ? or \ in it. Refactored queryRecentUsername definition into getRecentUsername def

--- get_up_to_date_usernames_members_lists_only_selenium.py
+++ get_up_to_date_usernames_members_lists_only_selenium.py
@@ -22,6 +22,67 @@ class UnexpectedFormatException(Exception):
     pass
 
 def getRecentUsername(username, driver):
+    
+    def queryRecentUsername(username, driver):
+    
+        def strip_and_upper(s):
+            s = s.strip()
+            s = s[0].upper() + s[1:] if len(s) > 1 else s.upper()
+            return s
+        
+        usernameURLVersion = username
+        if "?" in usernameURLVersion:
+            usernameURLVersion = usernameURLVersion.replace("?", "%3F")
+        if "\\" in usernameURLVersion:
+            usernameURLVersion = usernameURLVersion.replace("\\", "%5C")
+        
+        driver.get("https://en.wikipedia.org/wiki/User:" + usernameURLVersion)
+        usernameMatch = re.search('User:(.*) - Wikipedia', driver.title)
+        if not usernameMatch:
+            # look for redirect to talk-page
+            usernameMatch = re.search('User talk:(.*) - Wikipedia', driver.title)
+            if not usernameMatch:
+                return None
+            else:
+                return strip_and_upper(usernameMatch.group(1))
+        elif usernameMatch.group(1) != username:
+            # redirected to different user page
+            return strip_and_upper(usernameMatch.group(1))
+        else:
+            try:
+                userdoesnotexist_line = driver.find_element(By.CLASS_NAME, "mw-userpage-userdoesnotexist")
+            except NoSuchElementException:
+                userdoesnotexist_line = None
+            try:
+                move_line = driver.find_element(By.CLASS_NAME, "mw-logline-move")
+            except NoSuchElementException:
+                move_line = None
+            try:
+                rename_line = driver.find_element(By.CLASS_NAME, "mw-logline-renameuser")
+            except NoSuchElementException:
+                rename_line = None
+            if not userdoesnotexist_line and not move_line and not rename_line:
+                # should be a valid userpage
+                return username
+            elif rename_line:
+                username_list = rename_line.find_elements(By.TAG_NAME, "a")
+                if len(username_list) >= 6:
+                    return username_list[5].text
+                else:
+                    raise UnexpectedFormatException("Unexpected format: rename line")
+            elif move_line:
+                username_list = move_line.find_elements(By.TAG_NAME, "a")
+                if len(username_list) >= 6:
+                    new_userpage_title = username_list[5].text
+                    if new_userpage_title.startswith("User:"):
+                        return new_userpage_title[5:]
+                    else:
+                        raise UnexpectedFormatException("Unexpected format: move line: new userpage title")
+                else:
+                    raise UnexpectedFormatException("Unexpected format: move line")
+            else:
+                return None
+            
     recentUsername = queryRecentUsername(username, driver)
     oldUsername = username
     allNames = []
@@ -34,59 +95,7 @@ def getRecentUsername(username, driver):
             break
     return recentUsername
     
-def queryRecentUsername(username, driver):
-    
-    def strip_and_upper(s):
-        s = s.strip()
-        s = s[0].upper() + s[1:] if len(s) > 1 else s.upper()
-        return s
 
-    driver.get("https://en.wikipedia.org/wiki/User:" + username)
-    usernameMatch = re.search('User:(.*) - Wikipedia', driver.title)
-    if not usernameMatch:
-        # look for redirect to talk-page
-        usernameMatch = re.search('User talk:(.*) - Wikipedia', driver.title)
-        if not usernameMatch:
-            return None
-        else:
-            return strip_and_upper(usernameMatch.group(1))
-    elif usernameMatch.group(1) != username:
-        # redirected to different user page
-        return strip_and_upper(usernameMatch.group(1))
-    else:
-        try:
-            userdoesnotexist_line = driver.find_element(By.CLASS_NAME, "mw-userpage-userdoesnotexist")
-        except NoSuchElementException:
-            userdoesnotexist_line = None
-        try:
-            move_line = driver.find_element(By.CLASS_NAME, "mw-logline-move")
-        except NoSuchElementException:
-            move_line = None
-        try:
-            rename_line = driver.find_element(By.CLASS_NAME, "mw-logline-renameuser")
-        except NoSuchElementException:
-            rename_line = None
-        if not userdoesnotexist_line and not move_line and not rename_line:
-            # should be a valid userpage
-            return username
-        elif rename_line:
-            username_list = rename_line.find_elements(By.TAG_NAME, "a")
-            if len(username_list) >= 6:
-                return username_list[5].text
-            else:
-                raise UnexpectedFormatException("Unexpected format: rename line")
-        elif move_line:
-            username_list = move_line.find_elements(By.TAG_NAME, "a")
-            if len(username_list) >= 6:
-                new_userpage_title = username_list[5].text
-                if new_userpage_title.startswith("User:"):
-                    return new_userpage_title[5:]
-                else:
-                    raise UnexpectedFormatException("Unexpected format: move line: new userpage title")
-            else:
-                raise UnexpectedFormatException("Unexpected format: move line")
-        else:
-            return None
 
 #setup selenium webdriver
 chrome_options = Options()
