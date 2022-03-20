e08cf21a6e3196e8674ccc22fc39ca370192de6b
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-03-20 15:44:42 +0530

    drop minor column of new accounts contribs

---
+++ drop_minor_column_new_accounts.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 15:34:46 2022
+
+@author: madha
+"""
+
+import pandas as pd
+import os
+
+contribsDir = "/home/madhavso/wikipedia_data/top_editors/contributions"
+
+for file in os.listdir(contribsDir):
+    split = file.rsplit(".", maxsplit = 1)
+    if split[-1] != "csv":
+        continue
+    if split[0].startswith("m"):
+        continue
+    fileNo = int(split[0])
+    if fileNo <= 15994:
+        continue
+    
+    df = pd.read_csv(file, encoding = "utf-8")
+    df.drop(columns = "minor", inplace = True)
+    df.to_csv(file, index = False, encoding = "utf-8")
+    
